01cbdf29a85fc7f77eda9cb68b10f2d239b84d83
Alino22Git <hajra.alen@hotmail.com>
2024-02-28 17:41:03 +0100

    Removed Unused Bindings

--- TourPlanner/MainWindow.xaml.cs
+++ TourPlanner/MainWindow.xaml.cs
@@ -20,41 +20,13 @@ namespace TourPlanner
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow
     {
-        public string GreetingName { get; set; }
-        private string greetingMessage;
-        public string GreetingMessage
-        {
-            get => greetingMessage;
-            set
-            {
-                greetingMessage = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GreetingName)));
-            }
-        }
-            public event PropertyChangedEventHandler? PropertyChanged;
+       
         public MainWindow()
         {
             InitializeComponent();
         }
 
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Exit_Button(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine("Bye");
-            System.Environment.Exit(0);
-        }
-        private void Greet_Button(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine($"GreetingName is {GreetingName}");
-            GreetingMessage = $"Hello GreetingName {GreetingName}";
-            Debug.WriteLine($"Greeting Message is {GreetingName}");
-        }
     }
 }
