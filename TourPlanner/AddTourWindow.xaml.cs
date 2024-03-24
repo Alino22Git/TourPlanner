3d7bf5671fb8fa884114e1064ae935fa16f4407e
Alino22Git <hajra.alen@hotmail.com>
2024-03-24 16:54:12 +0100

    Created Window: AddTours
    Not implemented into the main program and has no data bindings.

---
+++ TourPlanner/AddTourWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace TourPlanner
+{
+    public partial class AddTourWindow : Window
+    {
+        public AddTourWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string location = LocationTextBox.Text;
+            string distance = DistanceTextBox.Text;
+            string description = DescriptionTextBox.Text;
+        }
+    }
+}
